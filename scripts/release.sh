d0f80b4585002e6b53bf6e67792f9a8027b142e8
Artem Chistyakov <chistyakov.artem@gmail.com>
2020-03-21 18:58:10 -0400

    Release on every push to master.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -eax
 
-VERSION=${1:-"alpha"}
-ARCHITECTURE=${2:-"x86_64-unknown-linux-musl"}
+ARCHITECTURE=${1:-"x86_64-unknown-linux-musl"}
 
 cargo build --release --target=${ARCHITECTURE}
 mkdir -p artifacts
-tar -C target/${ARCHITECTURE}/release -cf artifacts/lantern-${VERSION}_${ARCHITECTURE}.tar.gz lantern
+tar -C target/${ARCHITECTURE}/release -cf artifacts/lantern-${ARCHITECTURE}.tar.gz lantern
