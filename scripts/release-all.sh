59bce5a2a18b26c5da2cd8f253e15cd9c82779df
Artem Chistyakov <chistyakov.artem@gmail.com>
2020-06-06 20:19:31 -0400

    Switch to rust-tls.

--- scripts/release-all.sh
+++ scripts/release-all.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eax
 
-VERSION=${1:-"dev"}
-
-scripts/release.sh $VERSION x86_64-apple-darwin
-docker run --rm -v "$PWD":/usr/src/lantern -w /usr/src/lantern temochka/lantern-build:amd64 scripts/release.sh $VERSION x86_64-unknown-linux-musl
-docker run --rm -v "$PWD":/usr/src/lantern -w /usr/src/lantern temochka/lantern-build:i386 scripts/release.sh $VERSION i686-unknown-linux-musl
+# scripts/release.sh x86_64-apple-darwin
+docker run --rm -v "$PWD":/usr/src/lantern -w /usr/src/lantern temochka/lantern-build:amd64 scripts/release.sh x86_64-unknown-linux-musl
+# docker run --rm -v "$PWD":/usr/src/lantern -w /usr/src/lantern temochka/lantern-build:i386 scripts/release.sh i686-unknown-linux-musl
